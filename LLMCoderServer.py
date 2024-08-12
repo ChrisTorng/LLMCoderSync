779888c37983629b256b9dd309649a99971f5b05
ChrisTorng <ChrisTorng@hotmail.com>
2024-08-12 23:34:02 +0800

    必須依 instruction.zh-tw.md 指示的方式進行程式修改。請將 HTML 抽出為 LLMCoderServer.html，加上簡單的 LLMCoderServer.css 樣式。

--- LLMCoderServer.py
+++ LLMCoderServer.py
@@ -1,8 +1,9 @@
 import os
 from flask import Flask, render_template_string
+from flask import render_template
 from LLMCoderSync import should_ignore, get_ignore_patterns
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='.')
 
 def list_files(start_path):
     gitignore_patterns, claudeignore_patterns = get_ignore_patterns(start_path)
@@ -27,22 +28,8 @@ def list_files(start_path):
 def index():
     current_folder = os.getcwd()
     files = list_files(current_folder)
-    html = """
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>File List</title>
-    </head>
-    <body>
-        <h1>Files in Current Directory</h1>
-        <ul>
-        {% for file in files %}
-            <li>{{ file }}</li>
-        {% endfor %}
-        </ul>
-    </body>
-    </html>
-    """
+    return render_template('LLMCoderServer.html', files=files)
+    files = list_files(current_folder)
     return render_template_string(html, files=files)
 
 if __name__ == '__main__':
