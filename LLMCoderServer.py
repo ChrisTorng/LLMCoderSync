039a830fc9550b7389ab16b6078b501e720c295d
ChrisTorng <ChrisTorng@hotmail.com>
2024-08-13 09:02:01 +0800

    你剛剛的修改影響到 LLMCoderServer.py，請再詳細檢查兩個 py 檔中必要的修改，特別針對共用的部份。

--- LLMCoderServer.py
+++ LLMCoderServer.py
@@ -1,27 +1,26 @@
 import os
 from flask import Flask, render_template
-from LLMCoderSync import should_ignore, get_ignore_patterns, read_ignore_file
+from LLMCoderSync import should_ignore, get_ignore_patterns
 
 app = Flask(__name__, template_folder='.')
 
 def list_files(start_path):
-    gitignore_patterns, claudeignore_patterns = get_ignore_patterns(start_path)
-    syncignore_patterns = read_ignore_file(os.path.join(start_path, '.syncignore'))
+    gitignore_patterns, claudeignore_patterns, syncignore_patterns = get_ignore_patterns(start_path)
     file_list = []
     for root, dirs, files in os.walk(start_path, topdown=True):
         rel_root = os.path.relpath(root, start_path)
         
         # Filter out directories that should be ignored by .gitignore or .claudeignore
-        dirs[:] = [d for d in dirs if not should_ignore(os.path.join(rel_root, d), gitignore_patterns, claudeignore_patterns)]
+        dirs[:] = [d for d in dirs if not should_ignore(os.path.join(rel_root, d), gitignore_patterns, claudeignore_patterns, syncignore_patterns)]
         
         # Check if the current directory should be ignored by .gitignore or .claudeignore
-        if should_ignore(rel_root, gitignore_patterns, claudeignore_patterns):
+        if should_ignore(rel_root, gitignore_patterns, claudeignore_patterns, syncignore_patterns):
             continue
         
         for file in files:
             rel_path = os.path.join(rel_root, file)
-            if not should_ignore(rel_path, gitignore_patterns, claudeignore_patterns):
-                should_sync = not should_ignore(rel_path, syncignore_patterns, [])
+            if not should_ignore(rel_path, gitignore_patterns, claudeignore_patterns, syncignore_patterns):
+                should_sync = not should_ignore(rel_path, syncignore_patterns, [], [])
                 file_list.append((rel_path, should_sync))
     return file_list
 
